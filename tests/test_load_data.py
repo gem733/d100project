cc5440ff8c4e13fa13fcf7f9e0dade661ff5c867
gem733 <gm733@cam.ac.uk>
2025-12-09 12:18:44 +0000

    added init file for data module

---
+++ tests/test_load_data.py
@@ -0,0 +1,21 @@
+import pytest
+from data._load_data import load_data
+
+def test_load_data():
+
+    """
+    Test that load_data() loads the movies dataset correctly.
+    """
+
+    df = load_data()
+    
+    # Check that it returns a DataFrame
+
+    assert df is not None
+    assert not df.empty
+
+    # Check that all columns exist in df
+
+    expected_columns = ["budget", "genres", "id", "keywords", "original_language", "original_title", "overview", "popularity", "production_companies", "production_countries", "release_date", "revenue", "runtime", "spoken_languages", "status", "tagline", "title", "vote_average", "vote_count"]
+    for col in expected_columns:
+        assert col in df.columns
