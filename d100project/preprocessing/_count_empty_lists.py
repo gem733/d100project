e1630f696ad40e5ee8e9d7e3f7d708947d323424
gem733 <gm733@cam.ac.uk>
2025-12-10 12:25:39 +0000

    created function that counts empty lists

---
+++ d100project/preprocessing/_count_empty_lists.py
@@ -0,0 +1,14 @@
+def count_empty_lists(dataframe, column):
+    """
+    Count the number of empty lists in a specified column of a DataFrame.
+
+    Parameters:
+    dataframe (pd.DataFrame): The input DataFrame.
+    column_name (str): The name of the column to check for empty lists.
+
+    Returns:
+    int: The count of empty lists in the specified column.
+    """
+    empty_list_count = dataframe[column + "_list"].apply(lambda x: isinstance(x, list) and len(x) == 0).sum()
+    print("Number of missing values:", empty_list_count)
+    return empty_list_count
