318ba4fa0181eb1d7b70db9ae32358d33bab9a9f
gem733 <gm733@cam.ac.uk>
2025-12-11 14:37:14 +0000

    allowed the clean data fucntion to be able to run as it moves to the root dir

--- d100project/cleaning/_clean_data.py
+++ d100project/cleaning/_clean_data.py
@@ -1,4 +1,8 @@
+import sys
 from pathlib import Path
+
+project_root = Path(__file__).resolve().parents[2] 
+sys.path.insert(0, str(project_root)) 
 
 # Import your preprocessing functions
 from d100project.data._load_data import load_data
