c84070304db359d90da2b27d2e77e47fe11a02e9
gem733 <gm733@cam.ac.uk>
2025-12-11 12:11:11 +0000

    created remove missing data function

---
+++ d100project/cleaning/_remove_missing_data.py
@@ -0,0 +1,14 @@
+def removing_missing_data(df, columns):
+    """
+    Remove rows with missing data, NaNs, and rows with zero in specified columns.
+
+    Parameters:
+    df (DataFrame): The input DataFrame.
+    columns (list): List of column names to check for missing data.
+
+    Returns:
+    DataFrame: DataFrame with rows containing missing data in specified columns removed.
+    """
+    for column in columns:
+        df = df[df[column].notna() & (df[column] != 0)]
+    return df
